2af87eb70e8b08a1e6f1bec9d14ca0f612e19b79
He11oLiu <he11oliu830@gmail.com>
2017-08-14 10:38:08 +0800

    finish lab2

--- inc/memlayout.h
+++ inc/memlayout.h
@@ -156,7 +156,7 @@ typedef uint32_t pde_t;
  *
  * A second consequence is that the contents of the current page directory
  * will always be available at virtual address (UVPT + (UVPT >> PGSHIFT)), to
- * which uvpd is set in lib/entry.S.
+ * which uvpd is set in lib/entry.S. ??
  */
 extern volatile pte_t uvpt[];     // VA of "virtual page table"
 extern volatile pde_t uvpd[];     // VA of current page directory
